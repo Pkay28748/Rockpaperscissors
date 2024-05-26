34233441d65b3b4872c823bc2f39dbd62a860f47
Pkay28748 <pnkarthur@st.ug.edu.gh>
2024-05-26 06:26:59 -0400

    second task completed

---
+++ choice.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+
+
+    if(Math.random<= 0.3){
+        console.log('rock');
+    }
+    else if(Math.random <=0.6){
+        console.log('paper');
+    }
+    else {
+        console.log('scissors');
+    }
+}
+getComputerChoice();
