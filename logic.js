c615d22c8b8b9c0713dc56edeb230604f0ec4acf
Pkay28748 <pnkarthur@st.ug.edu.gh>
2024-05-30 11:11:02 -0400

    focus on rps.js file

---
+++ logic.js
@@ -0,0 +1,14 @@
+
+function getHumanChoice(){
+    let choice = prompt('Please enter your choice: ');
+
+    console.log(choice);
+}
+
+getHumanChoice();
+
+// for the browser environment, you create an html file and you create 
+// a button  that triggers the function when the button is clicked 
+
+// we use the onclick method to trigger the events 
+// i think this method is okay
---
+++ rps.js
@@ -0,0 +1,71 @@
+// We will be working on the final project where all the instruction
+// have been put into place and the final work has been showcased so stay with me 
+
+function getComputerchoice(){
+    let x = Math.floor(Math.random()* 3);
+
+    if (x === 0 ){
+        return "rock";
+    }
+
+    else if ( x === 1){
+        return "paper";
+    }
+
+    else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter any of the three choices").toLocaleLowerCase();
+
+    if ( choice === "rock" || choice==="paper" || choice==="scissors"){
+        console.log(choice);
+        return choice;
+    }
+    else{
+        console.log("You have any of these three choices")
+        return getComputerchoice();
+    }
+}
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore=0;
+
+    function playRound(humanChoice, computerChoice){
+     
+        if (humanChoice === computerChoice){
+            console.log("Its a draw");
+        }
+        else if(humanChoice==="rock" || computerChoice==="scissors"){
+            console.log(`You win ${humanChoice} beats ${computerChoice}`);
+            ++humanScore;
+        }
+
+        else if ( humanChoice=== "paper"||computerChoice==="rock"){
+            console.log(`You lost ${computerChoice} beats ${humanChoice}`);
+            ++computerScore;
+        }
+        else{
+            console.log("This combination is out of scope for this game");
+        }
+
+    }
+}
+// At this stage we will be playing the entire game from 1
+// Pass the methods of 1 and 2 as arguments in 3
+
+for (let a =0; a<5; a++){
+    playRound(getHumanChoice(),getComputerchoice())
+}
+
+if(humanScore > computerChoice){
+    return `You won with ${humanScore} points`
+}
+
+else {
+    return (humanScore === computerScore)? "its a draw match":"You lost totally";
+};
+
